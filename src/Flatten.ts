d81925d311f6ec748569b9461c3621abd6993a7e
Amine Ben hammou <webneat@gmail.com>
2022-01-02 19:44:52 +0100

    Add Depth to Flatten

--- src/Flatten.ts
+++ src/Flatten.ts
@@ -1,13 +1,21 @@
 import {Equal, Is} from './Is'
+import {Decrement} from './Decrement'
 
-export type Flatten<List extends any[][]> = List extends [infer First, ...infer Rest]
-  ? // @ts-expect-error `First` and `Rest` are not inferred correctly
-    [...First, ...Flatten<Rest>]
-  : []
+export type Flatten<List, Depth extends number = 1> = Depth extends 0
+  ? List
+  : List extends []
+  ? []
+  : List extends [infer First, ...infer Rest]
+  ? First extends any[]
+    ? [...Flatten<First, Decrement<Depth>>, ...Flatten<Rest, Depth>]
+    : [First, ...Flatten<Rest, Depth>]
+  : List
 
 type Tests = [
   Is<Equal<Flatten<[]>, []>>,
   Is<Equal<Flatten<[[1]]>, [1]>>,
   Is<Equal<Flatten<[[1], [2]]>, [1, 2]>>,
-  Is<Equal<Flatten<[[1], [2, 3], [[4]]]>, [1, 2, 3, [4]]>>
+  Is<Equal<Flatten<[[1], [[2, 3]], [[4]]]>, [1, [2, 3], [4]]>>,
+  Is<Equal<Flatten<[[1], [[2, 3]], [[4]]], 2>, [1, 2, 3, 4]>>,
+  Is<Equal<Flatten<[[1], [[2, 3]], [[4]]], 5>, [1, 2, 3, 4]>>
 ]
